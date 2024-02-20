d86b50506e86666e96d006557cf183e1ae873fab
Moishi Netzer <moishinetzer@gmail.com>
2024-02-20 14:54:32 +0000

    complete styling and initial project

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,15 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import "./tailwind.css";
+import { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: "https://fonts.cdnfonts.com/css/pokemon-solid" },
+  ];
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -15,7 +24,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body
+        style={{
+          fontFamily: "Comic Sans MS",
+          // fontFamily: "Pokemon Solid",
+          // WebkitTextStroke: "1px black",
+        }}
+        className="text-2xl bg-cyan-900 text-cyan-100 font-extralight tracking-widest"
+      >
         {children}
         <ScrollRestoration />
         <Scripts />
---
+++ app/routes/pokemon.tsx
@@ -0,0 +1,183 @@
+import { useEffect, useState } from "react";
+import { POKEMON_API_URL, Pokemon } from "scripts/api";
+
+export default function Index() {
+  const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
+
+  useEffect(() => {
+    document.title = "PokèCRApp";
+  }, []);
+
+  return selectedPokemon ? (
+    <SelectedPokemon
+      pokemon={selectedPokemon}
+      clearSelectedPokemon={() => setSelectedPokemon(null)}
+    />
+  ) : (
+    <AllPokemon setSelectedPokemon={setSelectedPokemon} />
+  );
+}
+
+function AllPokemon({
+  setSelectedPokemon,
+}: {
+  setSelectedPokemon: (p: Pokemon) => void;
+}) {
+  const [loading, setLoading] = useState(true);
+  const [allPokemon, setAllPokemon] = useState<Pokemon[] | null>(null);
+
+  const [page, setPage] = useState(1);
+
+  async function fetchPokemon(page: number) {
+    const pokemon = await fetch(
+      POKEMON_API_URL + "/pokemon" + `?page=${page}`
+    ).then((res) => res.json());
+    setAllPokemon(pokemon);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPokemon(page);
+  }, [page]);
+
+  if (loading || !allPokemon) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1 className="text-center p-8 text-yellow-500 text-4xl">PokèCRApp</h1>
+
+      <div className="flex justify-center items-center gap-8">
+        <button
+          onClick={() => {
+            setPage((page) => Math.max(page - 1, 1));
+          }}
+          className="bg-cyan-700 text-cyan-100 p-2 rounded-xl border border-cyan-200 w-36"
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => setPage((page) => page + 1)}
+          className="bg-cyan-700 text-cyan-100 p-2 rounded-xl border border-cyan-200 w-36"
+        >
+          Next
+        </button>
+      </div>
+
+      <ul className="flex flex-wrap p-8 gap-8 justify-center items-center">
+        {allPokemon.map((p) => (
+          <li
+            key={p.id}
+            className="relative bg-cyan-800 rounded-xl p-8 text-center"
+          >
+            {/* Favourite absolute top right corner star filled or unfilled */}
+            <span
+              className="absolute top-2 right-2 text-2xl"
+              style={{ color: p.favourite ? "yellow" : "white" }}
+            >
+              ★
+            </span>
+
+            <img src={p.image} alt={p.name} className="w-64" />
+            <h2 className="text-5xl">{p.name}</h2>
+            <button
+              className="mt-8 py-2 px-8 border-2 border-cyan-100 rounded-xl text-lg"
+              onClick={() => setSelectedPokemon(p)}
+            >
+              View
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function SelectedPokemon({
+  pokemon,
+  clearSelectedPokemon,
+}: {
+  pokemon: Pokemon;
+  clearSelectedPokemon: () => void;
+}) {
+  const [favourite, setFavourite] = useState(pokemon.favourite);
+
+  return (
+    <div className="text-center">
+      <h1 className="text-center p-8 text-yellow-500 text-4xl">PokèCRApp</h1>
+
+      <div className="relative p-16 rounded-3xl bg-cyan-800">
+        <h2 className="text-6xl">{pokemon.name}</h2>
+        <img src={pokemon.image} alt={pokemon.name} className="w-56 mx-auto" />
+
+        <div className="p-6 mx-auto bg-cyan-700 rounded-xl shadow-md flex items-center space-x-4">
+          <div className="mx-auto">
+            <p className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              {pokemon.type}
+            </p>
+            <p className="text-center mx-auto">
+              {pokemon.abilities.map((ability, index) => (
+                <span
+                  key={index}
+                  className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                >
+                  {ability}
+                </span>
+              ))}
+            </p>
+            <ul className="list-disc list-inside text-start mt-8">
+              <p className="">Stats:</p>
+              <li>
+                HP: <span className="font-bold">{pokemon.stats.hp}</span>
+              </li>
+              <li>
+                Attack:{" "}
+                <span className="font-bold">{pokemon.stats.attack}</span>
+              </li>
+              <li>
+                Defense:{" "}
+                <span className="font-bold">{pokemon.stats.defense}</span>
+              </li>
+              <li>
+                Favourite:{" "}
+                <span className="font-bold">{favourite ? "Yes" : "No"}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <button
+          onClick={clearSelectedPokemon}
+          className="mt-8 py-2 px-8 border-2 border-cyan-100 rounded-xl text-lg absolute top-2 left-8"
+        >
+          Back
+        </button>
+
+        <form method="post" className="absolute top-8 right-8">
+          <button
+            type="submit"
+            onClick={async (e) => {
+              e.preventDefault();
+
+              await fetch(
+                POKEMON_API_URL + `/pokemon/${pokemon.id}/favourite`,
+                {
+                  method: "POST",
+                }
+              );
+              setFavourite(!favourite);
+            }}
+          >
+            <span
+              className="text-5xl"
+              style={{ color: favourite ? "yellow" : "white" }}
+            >
+              ★
+            </span>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
